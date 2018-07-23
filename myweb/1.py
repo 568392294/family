7f58671d31eabdd286dac9ec2a8067422959e49b
15527481427 <568392294@qq.com>
2018-07-23 22:09:40 +0800

    enter

--- myweb/1.py
+++ myweb/1.py
@@ -1,6 +1,11 @@
 #coding=utf-8
 import urllib.request
 
-keywd='hello'
-url='http://www.baidu.com/s?wd='+keywd
+#keywd='hello'
+url='http://www.baidu.com'
 req=urllib.request.Request(url)
+data=urllib.request.urlopen(req).read()
+fhandle=open("D://python/html/4.html","wb")
+fhandle.write(data)
+print(data)
+fhandle.close()
